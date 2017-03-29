c3fa904fd2a3ce12abd95f19d6a2f9c3bcfc7958
Pablo Jurado <pjurado85@gmail.com>
2017-03-29 15:02:02 -0500

    finish exercise 6

--- exercise6/main.js
+++ exercise6/main.js
@@ -1,2 +1,18 @@
 // Write a small HTML and JavaScript program that outputs the current time like a clock.
 // This will most easily be accomplished using setInterval()
+function addCeroToSingleDigit (input) {
+  if (input.toString().length === 1) return '0' + input
+
+  return input
+}
+
+function renderTime (element) {
+  var clockElement = document.querySelector('div.clock')
+  var time = new Date()
+  var hour = addCeroToSingleDigit(time.getHours())
+  var minutes = addCeroToSingleDigit(time.getMinutes())
+  var seconds = addCeroToSingleDigit(time.getSeconds())
+  clockElement.innerHTML = hour + ':' + minutes + ':' + seconds
+}
+
+var oneSecondTimer = setInterval(renderTime, 1000)
