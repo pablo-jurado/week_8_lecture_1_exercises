3673fb4f04abba96047fedb0a1beb939318a482d
Pablo Jurado <pjurado85@gmail.com>
2017-03-29 14:12:38 -0500

    finish exercise 5

--- exercise5/startle.js
+++ exercise5/startle.js
@@ -6,6 +6,13 @@ reaction when started.
 */
 
 // startle.js
+function playGif () {
+  var bodyElement = document.querySelector('body')
+  bodyElement.classList.remove('star')
+  bodyElement.classList.add('speed')
+}
+
 function startle() {
   // todo: write code that produces a delayed reaction.
+  var delay3Sec = setTimeout(playGif, 3 * 1000)
 }
